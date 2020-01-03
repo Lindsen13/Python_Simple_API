13a3af8588c3acf151ba76ad57eba3e37d1a45fa
Lindsen13 <46817813+Lindsen13@users.noreply.github.com>
2020-01-03 14:32:27 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -12,12 +12,12 @@ url = base_url + '/'
 output = requests.get(url, auth=auth).json()
 print(output)
 
-#Let's populate the list with values from 1 to 12
+#Let's populate the list with values from 0 to 11
 for i in range(12):
     url = base_url + '/' + str(i)
     output = requests.get(url, auth=auth).json()
 
-#And let's retrieve all info. This should return a list from 0 to 12.
+#And let's retrieve all info. This should return a list from 0 to 11.
 url = base_url + '/'
 output = requests.get(url, auth=auth).json()
 print(output)
