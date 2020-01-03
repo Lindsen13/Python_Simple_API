db048f9ee50600b2dd1fbdb76ef60e56844182f6
Lindsen13 <46817813+Lindsen13@users.noreply.github.com>
2020-01-03 14:31:47 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,19 +1,23 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
+#Define authenticated user
 auth = HTTPBasicAuth('username', 'password')
+
+#Define base url
 base_url = 'http://localhost:5000'
 
+#Get all the items currently stored. If you just started the API, the list will be empty
 url = base_url + '/'
 output = requests.get(url, auth=auth).json()
 print(output)
 
+#Let's populate the list with values from 1 to 12
 for i in range(12):
     url = base_url + '/' + str(i)
-    print(url)
     output = requests.get(url, auth=auth).json()
 
-
-url = base_url + '/delete/10'
+#And let's retrieve all info. This should return a list from 0 to 12.
+url = base_url + '/'
 output = requests.get(url, auth=auth).json()
 print(output)
